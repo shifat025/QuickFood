7af9c5a8bc3eb64b992b752e972827a5c83ed161
Md Mim Shifat <shifat58201@gmail.com>
2025-03-22 22:52:16 +0600

    true

--- QuickFood/settings.py
+++ QuickFood/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fnpo1*)bnaj8(xyc%6l@=bc@eiaofz)@-j399tbsxrzr1qgs!9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://quickfood-x9fk.onrender.com', 'https://quick-food-frontend.vercel.app/']
